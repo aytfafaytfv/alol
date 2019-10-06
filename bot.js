40adb5dd1df57cf7757029bc55f4910f434b569e
aytfafaytfv <56179890+aytfafaytfv@users.noreply.github.com>
2019-10-06 02:20:25 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,7 +11,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : EX Clan ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`BC`,"http://twitch.tv/Death Shop")
+client.user.setGame(`!_Ofex_BC`,"http://twitch.tv/fares100f")
 client.user.setStatus("dnd")
 });
 
